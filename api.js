3c20c8b2d44d52cc9975d4b21269ce12a13258c7
b0c-beep <bocmarioadrian98@gmail.com>
2024-07-25 13:13:22 +0300

    still trying to make require dotenv config work

--- api.js
+++ api.js
@@ -1,19 +1,2 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + process.env.API_KEY + "&q=";
-
-const searchBox = document.querySelector(".searchbar input");
-const searchButton = document.querySelector(".searchbar button");
-
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city);
-    var data = await response.json();
-
-    document.querySelector(".result-card .city-name").innerHTML = data.name;
-    document.querySelector(".result-card .temp").innerHTML = Math.round(data.main.temp) + "°C";
-    
-}
-
-searchButton.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
