8ff196a3a63dd7f8851a0122b4910017746c1d9d
b0c-beep <bocmarioadrian98@gmail.com>
2024-07-25 15:01:03 +0300

    added error case

--- api.js
+++ api.js
@@ -9,24 +9,32 @@ async function checkWeather(city){
     const response = await fetch(apiUrl + city);
     var data = await response.json();
 
-    document.querySelector(".result-card .city-name").innerHTML = data.name;
-    document.querySelector(".result-card .temp").innerHTML = Math.round(data.main.temp) + "°C";  
-
-    switch (data.weather[0].main){
-        case "Clouds":
-            weatherIcon.src = "resources/clouds.svg";
-            break;
-        case "Rain":
-            weatherIcon.src = "resources/rain.svg";
-            break;
-        case "Clear": 
-            weatherIcon.src = "resources/clear.svg";
-            break;
-        default:
-            weatherIcon.src = "resources/clear.svg";
+    if(data.cod != 200){
+        document.querySelector(".result-card .city-name").innerHTML = "Error";
+        document.querySelector(".result-card .temp").style.visibility = "hidden";
+        weatherIcon.src = "resources/error.svg";
     }
+    else{
+        document.querySelector(".result-card .city-name").innerHTML = data.name;
+        document.querySelector(".result-card .temp").innerHTML = Math.round(data.main.temp) + "°C";  
 
-    document.querySelector(".result-card").style.visibility = "visible";
+        switch (data.weather[0].main){
+            case "Clouds":
+                weatherIcon.src = "resources/clouds.svg";
+                break;
+            case "Rain":
+                weatherIcon.src = "resources/rain.svg";
+                break;
+            case "Clear": 
+                weatherIcon.src = "resources/clear.svg";
+                break;
+            default:
+                weatherIcon.src = "resources/clear.svg";
+        }
+
+        document.querySelector(".result-card .temp").style.visibility = "visible";
+        document.querySelector(".result-card").style.visibility = "visible";
+    }
 }
 
 searchButton.addEventListener("click", () => {
